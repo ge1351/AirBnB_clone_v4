d08a7445b804ab3f3a0f5e35eb85f7928049ffdc
Getachew Alemayehu <105641499+ge1351@users.noreply.github.com>
2023-01-30 12:05:57 +0300

    Create 2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,32 @@
+const amenities = {};
+$(document).ready(function () {
+  $('#api_status').addClass('not_available');
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).prop('checked') === true) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    $('.amenities H4').text('');
+    let count = 0;
+    Object.entries(amenities).forEach(([key, value]) => {
+      if (count === 0) {
+        $('.amenities H4').append(value);
+      } else {
+        $('.amenities H4').append(', ');
+        $('.amenities H4').append(value);
+      }
+      count += 1;
+    });
+  });
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/'
+  }).done(data => {
+    if (data.status === 'OK') {
+      $('#api_status').toggleClass('available');
+    } else {
+      $('#api_status').toggleClass('not_available');
+    }
+  });
+});
